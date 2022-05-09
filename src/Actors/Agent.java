332fdeca5f832e955b84a9fbcecb9389642e2efa
viper23 <htonnycarlos@gmail.com>
2022-05-09 23:42:04 +0200

    added logic for agent and Student

--- src/Actors/Agent.java
+++ src/Actors/Agent.java
@@ -1,4 +1,47 @@
 package Actors;
 
-public class Agent {
+public class Agent extends Person{
+    private String username_agent;
+    private String password_agent;
+    private int id_agent;
+
+    // the constructor
+
+    public Agent(String username_agent, String password_agent, int id_agent){
+        super();
+        this.username_agent = username_agent;
+        this.password_agent = password_agent;
+        this.id_agent = id_agent;
+    }
+
+    // the empty constructor
+
+    public Agent(){}
+
+    // Getters and Setters
+
+
+    public String getUsername_agent() {
+        return username_agent;
+    }
+
+    public void setUsername_agent(String username_agent) {
+        this.username_agent = username_agent;
+    }
+
+    public String getPassword_agent() {
+        return password_agent;
+    }
+
+    public void setPassword_agent(String password_agent) {
+        this.password_agent = password_agent;
+    }
+
+    public int getId_agent() {
+        return id_agent;
+    }
+
+    public void setId_agent(int id_agent) {
+        this.id_agent = id_agent;
+    }
 }
--- src/Actors/Student.java
+++ src/Actors/Student.java
@@ -1,6 +1,61 @@
 package Actors;
 
+import Models.Class;
+import Models.Department;
+import Models.Faculty;
+
 public class Student extends Person{
     private int inscription_number;
+    private Faculty faculty_name;
+    private Department department_name;
+    private Class class_name;
 
+    // the constructor
+
+    public Student(int inscription_number, Faculty faculty_name, Department department_name, Class class_name){
+        super();
+        this.inscription_number = inscription_number;
+        this.faculty_name = faculty_name;
+        this.department_name = department_name;
+        this.class_name = class_name;
+    }
+
+    //the empty constructor
+
+    public Student(){}
+
+    // Getter and Setter
+
+
+    public int getInscription_number() {
+        return inscription_number;
+    }
+
+    public void setInscription_number(int inscription_number) {
+        this.inscription_number = inscription_number;
+    }
+
+    public Faculty getFaculty_name() {
+        return faculty_name;
+    }
+
+    public void setFaculty_name(Faculty faculty_name) {
+        this.faculty_name = faculty_name;
+    }
+
+    public Department getDepartment_name() {
+        return department_name;
+    }
+
+    public void setDepartment_name(Department department_name) {
+        this.department_name = department_name;
+    }
+
+    public Class getClass_name() {
+        return class_name;
+    }
+
+    public void setClass_name(Class class_name) {
+        this.class_name = class_name;
+    }
 }
