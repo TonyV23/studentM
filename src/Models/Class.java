7dfc6a522725016531587463d9cf9f7820701fb8
viper23 <htonnycarlos@gmail.com>
2022-05-10 22:48:18 +0200

    class attributes' correction

--- src/Models/Class.java
+++ src/Models/Class.java
@@ -3,14 +3,13 @@ package Models;
 public class Class {
     private String class_name;
     private Faculty faculty_name;
-    private Department department_class_name;
+
 
     // the constructor
 
-    public Class(String class_name, Faculty faculty_name, Department department_class_name){
+    public Class(String class_name, Faculty faculty_name){
         this.class_name = class_name;
         this.faculty_name = faculty_name;
-        this.department_class_name = department_class_name;
     }
 
     // the empty constructor
@@ -36,11 +35,4 @@ public class Class {
         this.faculty_name = faculty_name;
     }
 
-    public Department getDepartment_class_name() {
-        return department_class_name;
-    }
-
-    public void setDepartment_class_name(Department department_class_name) {
-        this.department_class_name = department_class_name;
-    }
 }
