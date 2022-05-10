29ac6de4f01d63db51aba2ac72ccf646eca829ee
viper23 <htonnycarlos@gmail.com>
2022-05-10 22:44:50 +0200

    student attributes' correction

--- src/Actors/Student.java
+++ src/Actors/Student.java
@@ -7,17 +7,15 @@ import Models.Faculty;
 public class Student extends Person{
     private int inscription_number;
     private Faculty faculty_name;
-    private Department department_name;
-    private Class class_name;
+    private Agent agent_name;
 
     // the constructor
 
-    public Student(String name, String surname, int age, char sex ,int inscription_number, Faculty faculty_name, Department department_name, Class class_name){
+    public Student(String name, String surname, int age, char sex ,int inscription_number, Faculty faculty_name,Agent agent_name){
         super(name, surname, age, sex);
         this.inscription_number = inscription_number;
         this.faculty_name = faculty_name;
-        this.department_name = department_name;
-        this.class_name = class_name;
+        this.agent_name = agent_name;
     }
 
     //the empty constructor
@@ -43,19 +41,11 @@ public class Student extends Person{
         this.faculty_name = faculty_name;
     }
 
-    public Department getDepartment_name() {
-        return department_name;
+    public Agent getAgent_name() {
+        return agent_name;
     }
 
-    public void setDepartment_name(Department department_name) {
-        this.department_name = department_name;
-    }
-
-    public Class getClass_name() {
-        return class_name;
-    }
-
-    public void setClass_name(Class class_name) {
-        this.class_name = class_name;
+    public void setAgent_name(Agent agent_name) {
+        this.agent_name = agent_name;
     }
 }
