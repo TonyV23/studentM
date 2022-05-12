a29c332ace6f88de524d8395c6ba476ee210b58d
viper23 <htonnycarlos@gmail.com>
2022-05-12 20:15:53 +0200

    added methods to handle class

--- src/Models/Class.java
+++ src/Models/Class.java
@@ -1,6 +1,11 @@
 package Models;
 
+import InputOutput.Io;
+
 import java.util.ArrayList;
+
+import static Models.Faculty.All_faculties_list;
+import static Models.Faculty.displayAllFaculties;
 
 public class Class {
     private String class_name;
@@ -15,7 +20,7 @@ public class Class {
         All_class_list.add(this);
     }
 
-    ArrayList<Class> All_class_list = new ArrayList<>();
+    static ArrayList<Class> All_class_list = new ArrayList<>();
 
     // the empty constructor
 
@@ -38,6 +43,28 @@ public class Class {
 
     public void setFaculty_name(Faculty faculty_name) {
         this.faculty_name = faculty_name;
+    }
+
+    // methods about handling departments
+
+    public static void createNewClass(){
+        displayAllFaculties();
+        new Class(Io.setString("Class name :"),All_faculties_list.get(Io.setINT("Number of faculty :")));
+    }
+
+    public static void updateClass(){
+        displayAllClass();
+        All_class_list.set(Io.setINT("Number of department to update :")-1,new Class(Io.setString("class name :"),All_faculties_list.get(Io.setINT("Number of faculty :"))));
+    }
+
+    public static void removeClass(){
+        displayAllClass();
+        All_class_list.remove(Io.setINT("Number of department to remove :")-1);
+        System.out.print("Updated List :" +All_class_list);
+    }
+
+    public static void displayAllClass(){
+        System.out.print("All class list :"+All_class_list);
     }
 
 }
